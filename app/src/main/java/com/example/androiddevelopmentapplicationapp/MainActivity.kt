d18952bf62bd6511dd6be0d92456f93ea3e9e626
Galina <sokolovagb@gmail.com>
2025-07-26 16:12:56 +0300

    RA-04 addToBackStack

--- app/src/main/java/com/example/androiddevelopmentapplicationapp/MainActivity.kt
+++ app/src/main/java/com/example/androiddevelopmentapplicationapp/MainActivity.kt
@@ -36,6 +36,7 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add<CategoriesListFragment>(R.id.mainContainer)
+            addToBackStack(null)
         }
     }
 }
