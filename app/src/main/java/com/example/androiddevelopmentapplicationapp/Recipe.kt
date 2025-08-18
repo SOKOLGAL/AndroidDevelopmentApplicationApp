b13d9bfc08ae16e2890698961ef8202f152fe5ef
Galina <sokolovagb@gmail.com>
2025-08-18 17:17:30 +0300

    RA-13 @parcelize in Category.kt Ingredient.kt Recipe.kt

--- app/src/main/java/com/example/androiddevelopmentapplicationapp/Recipe.kt
+++ app/src/main/java/com/example/androiddevelopmentapplicationapp/Recipe.kt
@@ -1,16 +1,13 @@
 package com.example.androiddevelopmentapplicationapp
-import android.annotation.SuppressLint
+
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
-@Suppress("DEPRECATED_ANNOTATION")
-@SuppressLint("ParcelCreator")
 @Parcelize
-
 data class Recipe(
     val id: Int,
     val title: String,
     val ingredients: List<Ingredient>,
     val method: List<String>,
-    val imageUrl: String,
-): Parcelable
+    val imageUrl: String
+) : Parcelable
